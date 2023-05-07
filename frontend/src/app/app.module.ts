ddf5455b540a77d63197cdc48c7374acdb434812
Gabriel Tarifa <gabrieltarifa@gmail.com>
2023-05-07 16:53:41 -0300

    Agrega validación básica al registro

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,9 @@ import { HomeComponent } from './views/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/views/signup/signup.component.ts
+++ frontend/src/app/views/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
+  formSignUp!: FormGroup;
+  wasSubmitted: boolean = false; // Flag para saber si el usuario envió el formulario
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formSignUp = this.formBuilder.group({
+      email: new FormControl('',  [Validators.email, Validators.required]),
+      password: new FormControl('',  Validators.required),
+      passwordConfirm: new FormControl('',  Validators.required),
+      firstName: new FormControl('',  Validators.required),
+      lastName: new FormControl('',  Validators.required),
+      address: new FormControl('',  Validators.required),
+    });
+  }
+
+  onSubmit() {
+    this.wasSubmitted = true;
+
+    if(this.formSignUp?.valid) {
+      // TODO: Inyectar servicio de SignUp y validar contra backend si devuelve 200 OK mostrar mensaje sino throw exception/error
+      console.log("Registro exitoso");
+    }
+  }
 }
