1cbacad6cc23f1908a4d92fc459c47bfa8b8bb7a
TomiiVaz <eliseovazquez01@gmail.com>
2023-05-26 17:13:52 -0300

    Login routeo

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignupComponent } from './views/signup/signup.component';
 import { HomeComponent } from './views/home/home.component';
 import { CartComponent } from './views/cart/cart.component';
+import { LoginComponent } from './views/login/login.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'registro', component: SignupComponent},
   {path: 'carrito', component: CartComponent},
+  {path: 'ingreso', component: LoginComponent},
 ];
 
 @NgModule({
