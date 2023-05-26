9400da9be38d99929536445ff289b83495ee565d
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2023-05-26 18:07:56 -0300

    primer config del back

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = require("./src/route/product.router");
+const app = express();
+const mongoose = require("mongoose")
+
+const port = 3000;
+const URI = "mongodb+srv://tallerweb2:OU1FiMJesXKK6SvO@carcash.gw7cpnl.mongodb.net/?retryWrites=true&w=majority"
+
+app.use(express.json());
+app.use("/api/products", router);
+
+mongoose
+    .connect(URI)
+    .then(()=>{
+        app.listen(port, () => {
+            console.log(`Servidor iniciado en el puerto ${port}`);
+          });
+    })
+    .catch((e)=>{
+        console.log(e);
+    })
+
+
+
+module.exports = app;
---
+++ backend/src/controller/product.controller.js
@@ -0,0 +1,5 @@
+const prueba = async (req, res) => {
+    res.send('¡Hola, mundo!');
+}
+
+module.exports = {prueba};
